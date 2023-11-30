d9b1b97a797f759f021d997645016b4d69c45f35
CelsoPlayMotta <celsodeoliveiramotta@gmail.com>
2023-11-29 22:05:31 -0300

    criando codigo

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let NomeHeroi ="celso"
+let nivel ="iniciante"
+let Xp= 2000
+switch(Xp){
+ case 1000:
+ nivel = "ferro"
+}
+ 
+console.log("O héroi de nome "+NomeHeroi,"esta no nivel "+nivel);
+
+
+
+
